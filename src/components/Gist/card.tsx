05d48f79406fcc1f288f907ee20aaa3ddae5aae8
Kaio Brito <kaio.brito92@gmail.com>
2023-02-08 09:57:39 -0300

    feat: List tags into the GistCard

--- src/components/Gist/card.tsx
+++ src/components/Gist/card.tsx
@@ -1,17 +1,34 @@
 import { GistType } from "@/networking/types";
+import { Chip } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import React from "react";
 
 type Props = {
   gist: GistType;
 };
 
 export const GistCard = ({ gist }: Props) => {
+  const tags = React.useMemo(() => {
+    if (!gist.files) {
+      return [];
+    }
+    const languages = new Set(
+      Object.values(gist.files)
+        .filter(Boolean)
+        .map((file) => file!.language)
+    );
+    return Array.from(languages);
+  }, [gist]);
+
   return (
     <Card>
       <CardContent>
         <Typography gutterBottom>{gist.description}</Typography>
+        {tags.map((tag, index) => (
+          <Chip key={index} label={tag} sx={{ mr: 1 }} />
+        ))}
       </CardContent>
     </Card>
   );
