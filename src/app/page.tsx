df0f00a3e7d8d5b95cd54df06fd6011c06deca57
Kaio Brito <kaio.brito92@gmail.com>
2023-02-08 09:03:55 -0300

    feat: Integrate useUsers with home autocomplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,30 @@
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import { useUsers } from "@/networking/useUsers";
+import { UsersFilters } from "@/networking/types";
+import { useState } from "react";
 
 export default function Home() {
+  const [filters, setFilters] = useState<UsersFilters>({ q: "" });
+  const { data } = useUsers(filters);
+
+  const searchUsers = (term: string) => {
+    setFilters({
+      q: term,
+    });
+  };
+
   return (
     <Stack sx={{ margin: 2 }}>
       <Autocomplete
         fullWidth
-        options={[]}
+        options={data?.data.items ?? []}
+        getOptionLabel={(option) => option.login}
         renderInput={(params) => <TextField {...params} label="Users" />}
+        onInputChange={(evt, inputValue) => {
+          searchUsers(inputValue);
+        }}
       />
     </Stack>
   );
