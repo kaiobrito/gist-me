b8e119c60214343b92cd265ad74f10f4fa0c8609
Kaio Brito <kaio.brito92@gmail.com>
2023-02-08 09:42:52 -0300

    feat: List gists of selected user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,18 @@ import TextField from "@mui/material/TextField";
 import { useUsers } from "@/networking/useUsers";
 import { UsersFilters } from "@/networking/types";
 import { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { useGists } from "@/networking/useGists";
 
 export default function Home() {
+  const [selecteUser, setSelectedUser] = useState<string>("");
   const [filters, setFilters] = useState<UsersFilters>({ q: "" });
+
   const { data } = useUsers(filters);
+  const { data: gists } = useGists(selecteUser);
 
   const searchUsers = (term: string) => {
     setFilters({
@@ -18,7 +26,7 @@ export default function Home() {
   };
 
   return (
-    <Stack sx={{ margin: 2 }}>
+    <Stack sx={{ margin: 2 }} spacing={4}>
       <Autocomplete
         fullWidth
         options={data?.data.items ?? []}
@@ -27,7 +35,23 @@ export default function Home() {
         onInputChange={(evt, inputValue) => {
           searchUsers(inputValue);
         }}
+        onChange={(evt, value) => {
+          setSelectedUser(value?.login ?? "");
+        }}
       />
+      <Grid container>
+        <Grid item xs={12} md={4}>
+          <Stack spacing={1}>
+            {gists?.data.map((gist) => (
+              <Card key={gist.id}>
+                <CardContent>
+                  <Typography gutterBottom>{gist.description}</Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </Stack>
+        </Grid>
+      </Grid>
     </Stack>
   );
 }
