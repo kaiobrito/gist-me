4d6cbcf952148a0336155a9b7de7db7b7e5e46a4
Kaio Brito <kaio.brito92@gmail.com>
2023-02-08 10:28:24 -0300

    feat: Debounce query and rehydrate from url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,69 @@ import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import { useUsers } from "@/networking/useUsers";
 import { UsersFilters } from "@/networking/types";
-import { useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import Grid from "@mui/material/Grid";
+import { debounce } from "@mui/material/utils";
 
 import { useGists } from "@/networking/useGists";
 import { GistCard } from "@/components/Gist/card";
+import { useRouter, useSearchParams } from "next/navigation";
+
+type AutocompleteType = {
+  id: string;
+  label: string;
+};
+
+const EMPTY_AUTOCOMPLETE: AutocompleteType = {
+  id: "",
+  label: "",
+};
 
 export default function Home() {
-  const [selecteUser, setSelectedUser] = useState<string>("");
+  const { push } = useRouter();
+  const query = useSearchParams();
+  const [inputValue, setInputValue] = useState(query.get("q") ?? "");
+
+  const [selectedUser, setSelectedUser] = useState<AutocompleteType>(
+    inputValue.length
+      ? { id: inputValue, label: inputValue }
+      : EMPTY_AUTOCOMPLETE
+  );
   const [filters, setFilters] = useState<UsersFilters>({ q: "" });
 
   const { data } = useUsers(filters);
-  const { data: gists } = useGists(selecteUser);
+  const { data: gists } = useGists(selectedUser?.id ?? "");
 
-  const searchUsers = (term: string) => {
-    setFilters({
-      q: term,
+  const search = useMemo(() => debounce(setFilters, 400), []);
+
+  useEffect(() => {
+    search({
+      q: inputValue,
     });
-  };
+  }, [inputValue, search]);
+
+  useEffect(() => {
+    if (selectedUser) {
+      push(`/?q=${selectedUser.id}`);
+    }
+  }, [push, selectedUser]);
 
   return (
     <Stack sx={{ margin: 2 }} spacing={4}>
       <Autocomplete
         fullWidth
-        options={data?.data.items ?? []}
-        getOptionLabel={(option) => option.login}
+        options={(data?.data.items ?? []).map((user) => ({
+          id: user.login,
+          label: user.login,
+        }))}
         renderInput={(params) => <TextField {...params} label="Users" />}
-        onInputChange={(evt, inputValue) => {
-          searchUsers(inputValue);
+        value={selectedUser}
+        inputValue={inputValue}
+        onInputChange={(evt, newValue) => {
+          setInputValue(newValue);
         }}
         onChange={(evt, value) => {
-          setSelectedUser(value?.login ?? "");
+          setSelectedUser(value ?? EMPTY_AUTOCOMPLETE);
         }}
       />
       <Grid container>
